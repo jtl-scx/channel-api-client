be03233884e97d8f2737663edebde6d297665c3b
Ronny Herrgesell <ronny.herrgesell@jtl-software.com>
2020-01-31 14:15:02 +0000

    EA-2568: Use EventType instead of strings

--- src/Api/Event/EventApi.php
+++ src/Api/Event/EventApi.php
@@ -8,8 +8,6 @@
 
 namespace JTL\SCX\Client\Channel\Api\Event;
 
-use DateTimeImmutable;
-use Exception;
 use GuzzleHttp\Exception\GuzzleException;
 use JTL\SCX\Client\Api\AuthAwareApiClient;
 use JTL\SCX\Client\Channel\Api\Event\Model\EventContainer;
@@ -17,14 +15,7 @@ use JTL\SCX\Client\Channel\Api\Event\Model\EventContainerList;
 use JTL\SCX\Client\Channel\Api\Event\Request\AcknowledgeEventIdListRequest;
 use JTL\SCX\Client\Channel\Api\Event\Request\GetEventListRequest;
 use JTL\SCX\Client\Channel\Api\Event\Response\GetSellerEventListResponse;
-use JTL\SCX\Client\Channel\Model\SellerEventOfferEnd;
-use JTL\SCX\Client\Channel\Model\SellerEventOfferNew;
-use JTL\SCX\Client\Channel\Model\SellerEventOrderCancelled;
-use JTL\SCX\Client\Channel\Model\SellerEventOrderConfirmed;
-use JTL\SCX\Client\Channel\Model\SellerEventOrderPayment;
-use JTL\SCX\Client\Channel\Model\SellerEventOrderShipping;
-use JTL\SCX\Client\Channel\Model\SellerEventTest;
-use JTL\SCX\Client\Channel\Model\SystemEventNotification;
+use JTL\SCX\Client\Channel\Helper\Event\EventType;
 use JTL\SCX\Client\Exception\RequestFailedException;
 use JTL\SCX\Client\JsonSerializer;
 use JTL\SCX\Client\ResponseDeserializer;
@@ -46,10 +37,10 @@ class EventApi
     }
 
     /**
+     * @param GetEventListRequest $request
      * @return GetSellerEventListResponse
-     * @throws RequestFailedException
-     * @throws Exception
      * @throws GuzzleException
+     * @throws RequestFailedException
      */
     public function getEventList(GetEventListRequest $request): GetSellerEventListResponse
     {
@@ -58,11 +49,12 @@ class EventApi
         $eventList = new EventContainerList();
 
         foreach ($data->eventList as $event) {
+            $eventType = new EventType($event->type);
             $eventContainer = new EventContainer(
                 $event->id,
-                new DateTimeImmutable($event->createdAt),
-                $event->type,
-                is_array($event->event) ? null : $this->createEventByType($event->type, $event->event)
+                new \DateTimeImmutable($event->createdAt),
+                $eventType->getValue(),
+                is_array($event->event) ? null : $this->createEventByType($eventType, $event->event)
             );
             $eventList->add($eventContainer);
         }
@@ -70,28 +62,12 @@ class EventApi
         return new GetSellerEventListResponse($eventList, $responseData->getStatusCode());
     }
 
-    private function createEventByType(string $type, \stdClass $data)
+    private function createEventByType(EventType $type, \stdClass $data)
     {
-        switch ($type) {
-            case 'System:Notification':
-                return $this->responseDeserializer->deserializeObject($data, SystemEventNotification::class);
-            case 'System:Test':
-                return $this->responseDeserializer->deserializeObject($data, SellerEventTest::class);
-            case 'Seller:Order.Confirmed':
-                return $this->responseDeserializer->deserializeObject($data, SellerEventOrderConfirmed::class);
-            case 'Seller:Order.Shipping':
-                return $this->responseDeserializer->deserializeObject($data, SellerEventOrderShipping::class);
-            case 'Seller:Order.Payment':
-                return $this->responseDeserializer->deserializeObject($data, SellerEventOrderPayment::class);
-            case 'Seller:Order.Cancelled':
-                return $this->responseDeserializer->deserializeObject($data, SellerEventOrderCancelled::class);
-            case 'Seller:Offer.End':
-                return $this->responseDeserializer->deserializeObject($data, SellerEventOfferEnd::class);
-            case 'Seller:Offer.New':
-                return $this->responseDeserializer->deserializeObject($data, SellerEventOfferNew::class);
+        if ($type->isUnknownEventType()) {
+            return $data;
         }
-
-        return $data;
+        return $this->responseDeserializer->deserializeObject($data, $type->getEventModelClass());
     }
 
     /**
--- src/Api/Event/Model/EventContainer.php
+++ src/Api/Event/Model/EventContainer.php
@@ -8,7 +8,7 @@
 
 namespace JTL\SCX\Client\Channel\Api\Event\Model;
 
-use JTL\SCX\Client\Channel\Model\SellerEventOfferEnd;
+use JTL\SCX\Client\Channel\Model\SellerEventOfferNew;
 use JTL\SCX\Client\Channel\Model\SellerEventOrderCancelled;
 use JTL\SCX\Client\Channel\Model\SellerEventOrderConfirmed;
 use JTL\SCX\Client\Channel\Model\SellerEventOrderPayment;
@@ -18,33 +18,11 @@ use JTL\SCX\Client\Channel\Model\SystemEventNotification;
 
 class EventContainer
 {
-    /**
-     * @var string
-     */
-    private $id;
-
-    /**
-     * @var \DateTimeImmutable
-     */
-    private $createdAt;
-
-    /**
-     * @var string
-     */
-    private $type;
-
-    /**
-     * @var SellerEventOfferEnd|SellerEventOrderCancelled|SellerEventOrderConfirmed|SellerEventOrderPayment|SellerEventOrderShipping|SellerEventTest|SystemEventNotification|null
-     */
+    private string $id;
+    private \DateTimeImmutable $createdAt;
+    private string $type;
     private $event;
 
-    /**
-     * EventContainer constructor.
-     * @param string $id
-     * @param \DateTimeImmutable $createdAt
-     * @param string $type
-     * @param SellerEventOfferEnd|SellerEventOrderCancelled|SellerEventOrderConfirmed|SellerEventOrderPayment|SellerEventOrderShipping|SellerEventTest|SystemEventNotification|null $event
-     */
     public function __construct(string $id, \DateTimeImmutable $createdAt, string $type, $event)
     {
         $this->id = $id;
@@ -53,32 +31,23 @@ class EventContainer
         $this->event = $event;
     }
 
-    /**
-     * @return string
-     */
     public function getId(): string
     {
         return $this->id;
     }
 
-    /**
-     * @return \DateTimeImmutable
-     */
     public function getCreatedAt(): \DateTimeImmutable
     {
         return $this->createdAt;
     }
 
-    /**
-     * @return string
-     */
     public function getType(): string
     {
         return $this->type;
     }
 
     /**
-     * @return SellerEventOfferEnd|SellerEventOrderCancelled|SellerEventOrderConfirmed|SellerEventOrderPayment|SellerEventOrderShipping|SellerEventTest|SystemEventNotification|null
+     * @return SellerEventOrderCancelled|SellerEventOrderConfirmed|SellerEventOrderPayment|SellerEventOrderShipping|SellerEventTest|SystemEventNotification|SellerEventOfferNew|null
      */
     public function getEvent()
     {
