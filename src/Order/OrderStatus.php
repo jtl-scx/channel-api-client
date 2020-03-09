5530c24bc481390a0ef6aa18e84f006b8558e7b2
Ronny Herrgesell <ronny.herrgesell@jtl-software.com>
2020-03-09 10:20:19 +0000

    add OrderStatus Enum

---
+++ src/Order/OrderStatus.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+/**
+ * This File is part of JTL-Software
+ *
+ * User: avermeulen
+ * Date: 2020-02-27
+ */
+
+namespace JTL\SCX\Client\Channel\Order;
+
+use MyCLabs\Enum\Enum;
+
+/**
+ * Class OrderStatus
+ * @method static OrderStatus CREATED()
+ * @method static OrderStatus ACCEPTED()
+ * @method static OrderStatus SHIPPED()
+ * @method static OrderStatus PARTIALLY_SHIPPED()
+ * @method static OrderStatus CANCELED_BY_SELLER()
+ * @method static OrderStatus CANCELED_BY_BUYER()
+ */
+class OrderStatus extends Enum
+{
+    public const CREATED = 'CREATED';
+    public const ACCEPTED = 'ACCEPTED';
+    public const SHIPPED = 'SHIPPED';
+    public const PARTIALLY_SHIPPED = 'PARTIALLY_SHIPPED';
+    public const CANCELED_BY_SELLER = 'CANCELED_BY_SELLER';
+    public const CANCELED_BY_BUYER = 'CANCELED_BY_BUYER';
+}
