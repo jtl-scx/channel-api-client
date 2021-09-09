5c2dffe6e40e6e6a1f84848dccc71f0b0d32bfcf
Ronny Herrgesell <ronny.herrgesell@jtl-software.com>
2021-09-09 09:31:28 +0000

    EA-3910: Add missing code coverage

---
+++ tests/Api/Order/Response/RequestOrderCancellationResponseTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace JTL\SCX\Client\Channel\Api\Order\Response;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \JTL\SCX\Client\Channel\Api\Order\Response\RequestOrderCancellationResponse
+ */
+class RequestOrderCancellationResponseTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function it_consider_request_successful_on_http_code_201(): void
+    {
+        $sut = new RequestOrderCancellationResponse(201);
+        $this->assertTrue($sut->isSuccessful());
+    }
+
+    /**
+     * @test
+     */
+    public function it_consider_request_failed_on_http_code_not_equals_201(): void
+    {
+        $sut = new RequestOrderCancellationResponse(200);
+        $this->assertFalse($sut->isSuccessful());
+    }
+}
